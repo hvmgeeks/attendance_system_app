82ccca336e250b1ee4f439bf09ae35e0ee616905
hvm geeks <hvmgeeks@gmail.com>
2024-01-04 01:20:35 +0300

    new

--- Home.py
+++ Home.py
@@ -4,7 +4,7 @@ import streamlit as st
 
 st.set_page_config(page_title='Attendance System',layout='wide')
 
-st.header('Attendance System using Face Recognition')
+st.header('St Joseph kibada face recognition')
 
 with st.spinner("Loading Models and Conneting to Redis db ..."):
     import face_rec
